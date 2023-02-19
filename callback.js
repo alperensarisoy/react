3278b857fe5ba95c3bc705cb643aef9091fd2739
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2023-02-19 20:37:54 +0300

    Update callback.js

--- callback.js
+++ callback.js
@@ -60,12 +60,34 @@
 
 // ************************************************
 
-async function getData(){
-    const users=await (await fetch("https://jsonplaceholder.typicode.com/users")).json();
+// async function getData(){
+//     const users=await (await fetch("https://jsonplaceholder.typicode.com/users")).json();
 
-    const post1=await (await fetch("https://jsonplaceholder.typicode.com/posts/1")).json();
+//     const post1=await (await fetch("https://jsonplaceholder.typicode.com/posts/1")).json();
 
-    const post2=await (await fetch("https://jsonplaceholder.typicode.com/posts/2")).json();
+//     const post2=await (await fetch("https://jsonplaceholder.typicode.com/posts/2")).json();
+
+
+
+
+//     console.log("users",users);
+//     console.log("post1",post1);
+//     console.log("post2",post2);
+// }
+
+// getData();
+
+// ********************************************
+
+import axios from "axios";
+
+
+(async ()=>{
+    const {data:users}=await axios("https://jsonplaceholder.typicode.com/users");
+
+    const {data:post1}=await axios("https://jsonplaceholder.typicode.com/posts/1");
+
+    const {data:post2}=await axios("https://jsonplaceholder.typicode.com/posts/2");
 
 
 
@@ -73,6 +95,4 @@ async function getData(){
     console.log("users",users);
     console.log("post1",post1);
     console.log("post2",post2);
-}
-
-getData();
+})();
